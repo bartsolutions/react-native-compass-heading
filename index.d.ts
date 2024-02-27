974189a0e930caefb966b376942a8e3c18a12a00
gongdao123 <gongdao123@gmail.com>
2024-02-27 14:30:55 +0800

    update type defination

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,5 @@ declare module 'react-native-compass-heading' {
   export const start: (
     threshold: number,
     callback: ({ heading: number, accuracy: number }) => void
-  ) => Promise<boolean>;
-
-  export const stop: () => Promise<void>;
-
-  export const hasCompass: () => Promise<boolean>;
+  ) => () => void;;
 }
